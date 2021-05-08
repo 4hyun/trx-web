cb60d077479717915b5712c60a2f7f8fbd91fdfd
Sa Hyun Hwang <4hyun8@gmail.com>
2021-05-08 17:13:36 +0900

    update menu

--- web-next/components/Menu/data.js
+++ web-next/components/Menu/data.js
@@ -1,6 +1,6 @@
 const menuItemMapper = ([id, href, label]) => ({ id, href, label })
 export const defaultMenuList = [
   [1, "/", "Collections"],
-  [2, "/", "Products"],
+  [2, "/", "Flavors"],
   [3, "/find-store", "Find Stores"],
 ].map(menuItemMapper)
