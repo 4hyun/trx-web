5c1ba966fc3de8ad19cd5fd098117a5983003eb5
Sa Hyun Hwang <4hyun8@gmail.com>
2022-08-31 14:31:01 +0900

    remove cms from git

--- cms/admin/src/containers/AuthPage/components/Logo/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import LogoTRX from "../../../../assets/images/logo_strapi.png";
-import Img from "./Img";
-
-const Logo = () => <Img src={LogoTRX} alt="tunaaaaroom xtracts logo" />;
-
-export default Logo;
--- cms/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '56f96c35-0a6f-4f5e-b440-9f3613367421'
-};
